87749d729e4a2c85b0219e07f3815e0159427c46
hamzanth <irekpitaanthony10@gmail.com>
2024-02-05 04:59:57 +0100

    add whitenoise and collect static files

--- btre/settings.py
+++ btre/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -153,6 +154,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, "btre/static")
 ]
